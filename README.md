bb27c996faedb45cb327c56af8ee9f7adae32054
kjakegibson <kjakegibson@hotmail.com>
2016-09-08 09:53:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# musicapi
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/BandController.js
@@ -0,0 +1,11 @@
+/**
+ * BandController
+ *
+ * @description :: Server-side logic for managing bands
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
