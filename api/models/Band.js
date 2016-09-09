863d93471bcf70b10d67e8a21a2c90a0d1a6cf02
kjakegibson <kjakegibson@hotmail.com>
2016-09-09 16:08:58 -0400

    final commit

--- api/models/Band.js
+++ api/models/Band.js
@@ -8,6 +8,37 @@
 module.exports = {
 
   attributes: {
+  	bandName: {
+  		type: 'string',
+  		required: true
+  	},
+  	genre: {
+  		type: 'string',
+  		required: true
+  	},
+  	subGenre: {
+  		type: 'string',
+  		defaultsTo: 'No Sub-Genre Added'
+  	},
+  	countryOfOrigin: {
+  		type: 'string',
+  		defaultsTo: 'No Country of Origin Added'
+  	},
+  	yearFormed: {
+  		type: 'string',
+  		defaultsTo: 'Year Formed Unknown'
+  	},
+  	lyricalThemes: {
+  		type: 'text',
+  		defaultsTo: 'No Lyrical Themes Added'
+  	},
+  	active: {
+  		type: 'string',
+  		enum: [1,0],
+  		required: true
+  	}
+
+
 
   }
 };
